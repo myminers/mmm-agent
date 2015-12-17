3577fdceb642afa3ec8a29649739bae68c399723
MyMiners.net <31168757+myminers@users.noreply.github.com>
2015-12-17 16:18:11 +0100

    Comments

--- lib/mmm-agent/settings.rb
+++ lib/mmm-agent/settings.rb
@@ -1,13 +1,9 @@
 require 'yaml'
 
 module Settings
-  # again - it's a singleton, thus implemented as a self-extended module
+  # Singleton class
   extend self
 
-  # This is the main point of entry - we call Settings.load! and provide
-  # a name of the file to read as it's argument. We can also pass in some
-  # options, but at the moment it's being used to allow per-environment
-  # overrides in Rails
   def load!(filename)
     @config = YAML::load_file(filename)
   end
