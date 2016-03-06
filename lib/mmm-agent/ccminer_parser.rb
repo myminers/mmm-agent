0863f43decd3c953a70a41f26d7c3fc837fda377
MyMiners.net <31168757+myminers@users.noreply.github.com>
2016-03-06 21:03:01 +0100

    Parse the GPU miner output lines

---
+++ lib/mmm-agent/ccminer_parser.rb
@@ -0,0 +1,17 @@
+class MmmAgent::MiningOperation
+
+  def self.parse(line, host, log)
+    case
+    when /GPU #(?<gpuid>\d).+, (?<hash>\d+)/ =~ line
+      # [2016-02-13 21:54:11] GPU #1: GeForce GTX 970, 12696 (T= 57C F= 56% C=1404/3004)
+      id = gpuid.to_i
+      rate = hash.to_i * 1000 # Convert to hash/s
+      host.gpu[id].update_stats # Get T°, power and resource usage from nvidia_smi
+      host.gpu[id].store_hashrate(rate)
+    else
+      clean_line = line.sub(/\A\[.+\]/, '')
+      log.info "Miner output: #{clean_line}"
+    end
+  end
+
+end
