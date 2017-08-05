c31c3b1747c0a72aa77e3a766a65ce8ad7b7428f
MyMiners.net <31168757+myminers@users.noreply.github.com>
2017-08-05 15:48:22 +0200

    Tell which GPU we're talking about in the logs

--- lib/mmm-agent/mining_operation.rb
+++ lib/mmm-agent/mining_operation.rb
@@ -10,13 +10,13 @@ class MmmAgent::MiningOperation
 
   def update( raw_data )
     if @raw_data == raw_data # No change
-      Log.info 'No change in mining command'
+      Log.info 'GPU##{@device.id} No change in mining command'
     elsif is_valid(raw_data)
       @raw_data = raw_data
-      Log.notice "Switching to '#{readable_command}'"
+      Log.notice "GPU##{@device.id} Switching to '#{readable_command}'"
       if !@pid.nil?
         begin
-          Log.info "Stopping the previous miner..."
+          Log.info "GPU##{@device.id} Stopping the previous miner..."
           Process.kill('INT', @pid)
           Process.wait(@pid)
         rescue PTY::ChildExited
@@ -25,8 +25,8 @@ class MmmAgent::MiningOperation
         @host.clear_statistics # Ignore the last stats output, as we are probably changing algorithm
       end
     else
-      Log.warning "Received invalid command, #{raw_data.to_s}"
-      Log.warning "Staying on: '#{readable_command}'"
+      Log.warning "GPU##{@device.id} Received invalid command, #{raw_data.to_s}"
+      Log.warning "GPU##{@device.id} Staying on: '#{readable_command}'"
     end
   end
   
@@ -48,32 +48,32 @@ class MmmAgent::MiningOperation
     #Infinite loop: if the miner stops, we restart it
     while true
       begin
-        Log.notice "Starting the miner..."
+        Log.notice "GPU##{@device.id} Starting the miner..."
         PTY.spawn( miner, algo, stratum, username, password, '--no-color' ) do |stdout, stdin, pid|
           begin
             @pid = pid
-            Log.debug "Miner running with PID #{@pid}"
+            Log.debug "GPU##{@device.id} Miner running with PID #{@pid}"
             stdout.each { |line| MmmAgent::CcminerParser::parse(line, @host) }
           rescue Errno::EIO
             # If the miner stops without closing stdout properly, we get this error.
             # We don't care about it, we just want to start another process...
-            Log.debug "Miner stopped output"
+            Log.debug "GPU##{@device.id} Miner stopped output"
           rescue Interrupt
             # We received an interrupt (ctrl-C or kill)
             # Stop the miner properly and exit
-            Log.info "Stopping the miner"
+            Log.info "GPU##{@device.id} Stopping the miner"
             begin
               Process.kill('INT', @pid)
               Process.wait(@pid)
             rescue PTY::ChildExited
               # Just wait for the process to stop
             end
-            Log.info "Miner stopped safely, stopping mmm-agent"
+            Log.info "GPU##{@device.id} Miner stopped safely, stopping mmm-agent"
             return
           end
         end
       rescue PTY::ChildExited
-        Log.debug "Miner process exited"
+        Log.debug "GPU##{@device.id} Miner process exited"
       end
     end
   end 
@@ -87,63 +87,38 @@ class MmmAgent::MiningOperation
   def is_valid( raw_data )
     m = raw_data['what_to_mine']
     if m['miner'] !~ /\A[a-z0-9\-\.]+\z/
-      Log.warning("Invalid miner name: #{m['miner']}")
+      Log.warning("GPU##{@device.id} Invalid miner name: #{m['miner']}")
       return false
     end
     if !system("which #{m['miner']} > /dev/null 2>&1")
-      Log.warning("Miner missing: #{m['miner']}")
+      Log.warning("GPU##{@device.id} Miner missing: #{m['miner']}")
       return false
     end
     if m['device'] !~ /\A[0-9]+\z/
-      Log.warning("Invalid device number: #{m['device']}")
+      Log.warning("GPU##{@device.id} Invalid device number: #{m['device']}")
       return false
     end
     if m['algo'] !~ /\A[a-z0-9\-\/]+\z/
-      Log.warning("Invalid algo: #{m['algo']}")
+      Log.warning("GPU##{@device.id} Invalid algo: #{m['algo']}")
       return false
     end
     if m['stratum'] !~ /\A[a-z0-9\-\:\.]+\z/
-       Log.warning("Invalid stratum: #{m['stratum']}")
+       Log.warning("GPU##{@device.id} Invalid stratum: #{m['stratum']}")
        return false
     end
     if m['username'] !~ /\A[a-zA-Z0-9\-\.]+\z/
-       Log.warning("Invalid username: #{m['username']}")
+       Log.warning("GPU##{@device.id} Invalid username: #{m['username']}")
        return false
     end
     if m['password'] !~ /\A[a-zA-Z0-9\-\.=]+\z/
-       Log.warning("Invalid password: #{m['password']}")
+       Log.warning("GPU##{@device.id} Invalid password: #{m['password']}")
        return false
     end
     if m['command'] !~ /\A[a-zA-Z0-9\-\.=_ ]+\z/
-      Log.warning("Invalid command: #{m['command']}")
+      Log.warning("GPU##{@device.id} Invalid command: #{m['command']}")
       return false
     end
     return true
-  end
-  
-  def miner
-    case @raw_data['algo']
-    when "neoscrypt"
-      "ccminer-djm"
-    else
-      "ccminer-sp"
-    end
-  end
-  
-  def algo
-    "--algo=#{@raw_data['algo']}"
-  end
-  
-  def stratum
-    "--url=stratum+tcp://#{@raw_data['stratum']}"
-  end
-  
-  def username
-    "--user=#{@raw_data['username']}"
-  end
-  
-  def password
-    "--pass=#{@raw_data['password']}"
   end
 
   def stratum_host( stratum )
