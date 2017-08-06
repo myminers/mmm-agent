71d30ccb5f98c509009ad132d2c79332e7cd96fa
MyMiners.net <31168757+myminers@users.noreply.github.com>
2017-08-06 18:51:09 +0200

    Improve hashrate extraction

--- lib/mmm-agent/miner_parser.rb
+++ lib/mmm-agent/miner_parser.rb
@@ -2,11 +2,12 @@ class MmmAgent::MinerParser
 
   def self.parse(line, gpu)
     case line
-    when /GPU #\d.+, ([\d\.]+) ([A-Za-z])[Hh]\/s/
+    when /GPU #\d.+, ([\d\.]+) ([A-Za-z]?[Hh])\/s/
       # [2016-02-13 21:54:11] GPU #1: GeForce GTX 970, 12696 (T= 57C F= 56% C=1404/3004)
       value = $1
       unit  = $2
-      gpu.hashrate = value.to_i * MmmAgent::MinerParser::multiplier(unit.downcase) # Convert to hash/s
+      rate = value.to_f * MmmAgent::MinerParser::multiplier(unit.downcase) # Convert to hash/s
+      gpu.hashrate = rate.to_i
     when /GPU\d: (\d+) /
       # GPU0: 410 Sol
       gpu.hashrate = $1.to_i
@@ -16,7 +17,7 @@ class MmmAgent::MinerParser
   end
 
   def self.multiplier(letter)
-    units = [nil, 'k', 'm', 'g', 't', 'p', 'e', 'z', 'y']
+    units = [nil, 'kh', 'mh', 'gh', 'th', 'ph', 'eh', 'zh', 'yh']
     orders = Hash[units.map.with_index.to_a]
     units.include?(letter) ? 1000 ** orders[letter] : 1
   end
